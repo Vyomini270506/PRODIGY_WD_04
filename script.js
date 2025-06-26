32ead7b1d39a80ac8be70d498d2c7c7501060813
Vyomini Joshi <u24cs070@coed.svnit.ac.in>
2025-06-26 18:52:06 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-cb=document.getElementById("contact-b")
-cb.addEventListener(click,()=>{
-    foot=document.getElementById("foot")
-    foot.classList.remove('ani')
-    void foot.offsetWidth;
-    foot.classList.add('ani')
-})
